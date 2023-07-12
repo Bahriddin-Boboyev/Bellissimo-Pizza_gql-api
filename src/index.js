1c489b0fa65c4ad5855fb1698dba9b79966aed57
scoderr <BahriddinBoboyev997@gmail.com>
2023-07-12 17:10:24 +0500

    some changes were made, small bugs were fixed and the database was configured

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import http from "http";
+import express from "express";
+import cors from "cors";
+import { buildGraphQLServer } from "./graphql/index.js";
+import config from "./shared/config/index.js";
+
+const app = express();
+const httpServer = http.createServer(app);
+
+app.use(express.json());
+app.use(cors());
+
+const { server, serverMiddleware } = buildGraphQLServer(httpServer);
+
+app.use("/gql", serverMiddleware);
+await server.start();
+
+const PORT = config.PORT || 8080;
+httpServer.listen(PORT, () => {
+  console.log(`http://localhost:${PORT}`);
+});
